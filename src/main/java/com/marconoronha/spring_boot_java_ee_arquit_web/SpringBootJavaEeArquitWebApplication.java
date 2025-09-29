0b2a3c52abe51c55ecf4a87b78b7daf32b9c1d2c
Marco Noronha <marcoapnoronha@hotmail.com>
2025-09-29 14:12:24 -0300

    Commit inicial

---
+++ src/main/java/com/marconoronha/spring_boot_java_ee_arquit_web/SpringBootJavaEeArquitWebApplication.java
@@ -0,0 +1,13 @@
+package com.marconoronha.spring_boot_java_ee_arquit_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavaEeArquitWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJavaEeArquitWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marconoronha/spring_boot_java_ee_arquit_web/SpringBootJavaEeArquitWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marconoronha.spring_boot_java_ee_arquit_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJavaEeArquitWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
